ce5be5874d505c158badf548f7b7a0e10039cbf2
Jinyoung Song <songdh21@naver.com>
2022-06-27 15:14:50 +0900

    ✨[Chap5] Mapping Done

---
+++ SpringBoot/cluv_projects/src/main/java/com/flareloop/cluv2shop/entity/Order.java
@@ -0,0 +1,31 @@
+package com.flareloop.cluv2shop.entity;
+
+import com.flareloop.cluv2shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    private LocalDateTime orderDate; //주문일
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+}
